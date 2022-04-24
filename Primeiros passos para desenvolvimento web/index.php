b6ffc6ff41d5cc197c2a1d0b023e79bbb2f3ec02
Fernandognu <fernandogdac@hotmail.com>
2022-04-24 13:03:06 -0300

    PHP Primeiro Codigo

---
+++ Primeiros passos para desenvolvimento web/index.php
@@ -0,0 +1,15 @@
+<html>
+    
+    <head>
+        <title>Meu Primeiro site em PHP! Woohoo!</title>
+    </head>
+
+    <body>
+        <?php
+            for ( $i = 0 ; $i <= 10 ; $i++ ) {
+                print( "Linha numero " . $i . "<br />");
+            }
+        ?>
+    </body>
+
+</html>
